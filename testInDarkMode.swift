486af96e058a07e03462a348770ffff2520d2b92
comfortabletriphazard <71904259+comfortabletriphazard@users.noreply.github.com>
2023-02-04 10:50:16 +0000

    Create testInDarkMode.swift

---
+++ testInDarkMode.swift
@@ -0,0 +1,11 @@
+//Replace your last struct on the ContentView with this code:
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        //dark mode:
+            .colorScheme(.dark)
+            .background(Color.black)
+    }
+}
